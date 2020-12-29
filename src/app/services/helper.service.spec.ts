10cb59390c095417f43accac72d1d22a11891ad2
yinnping <ciaoing@livemail.tw>
2020-12-29 15:58:27 +0800

    Fix all broken test

--- src/app/services/helper.service.spec.ts
+++ src/app/services/helper.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HelperService } from './helper.service';
 
 describe('HelperService', () => {
   let service: HelperService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HelperService);
   });
 
